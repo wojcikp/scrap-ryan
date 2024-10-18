df5b0b9f0a9bb0f43b6fc66f0f3eddfc2beefefe
Paweł Wójcik <avedonmedia@gmail.com>
2024-10-18 19:22:48 +0200

    refactor: move logics to separate functions

--- main.go
+++ main.go
@@ -16,26 +16,35 @@ import (
 )
 
 const (
-	lookForwardInMonths = 6
+	lookForwardInMonths = 5
+	offersPerMonth      = 5
 	chopinAirportCode   = "WAW"
 	modlinAirportCode   = "WMI"
 	alicanteAirportCode = "ALC"
 )
 
 func main() {
+	chatId, botToken := os.Args[1], os.Args[2]
 	now := time.Now()
 	currentYear, currentMonth, _ := now.Date()
 	currentLocation := now.Location()
 	startDate := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
 	endDate := startDate.AddDate(0, lookForwardInMonths, -1)
 
-	chatId, botToken := os.Args[1], os.Args[2]
+	warsawToAlicanteFares := getWarsawToAlicanteFlights(startDate, endDate)
+	alicanteToWarsawFares := getAlicanteToWarsawFlights(startDate, endDate)
+	convertEURtoPLN(&alicanteToWarsawFares)
 
+	flightsToCompare := getFlightsToCompare(warsawToAlicanteFares, alicanteToWarsawFares)
+	message := buildMessage(now, flightsToCompare)
+	sendMessageToTelegram(message, botToken, chatId)
+}
+
+func getWarsawToAlicanteFlights(startDate, endDate time.Time) []Fare {
 	var modlinToAlicante FlightResponse
 	var chopinToAlicante FlightResponse
-	var alicanteToModlin FlightResponse
-	var alicanteToChopin FlightResponse
 	var flightsData []byte
+
 	flightsData = getRyanFlights(modlinAirportCode, alicanteAirportCode, startDate, endDate)
 	err := json.Unmarshal([]byte(flightsData), &modlinToAlicante)
 	if err != nil {
@@ -46,8 +55,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Error unmarshalling JSON:", err)
 	}
+	return append(modlinToAlicante.Fares, chopinToAlicante.Fares...)
+}
+
+func getAlicanteToWarsawFlights(startDate, endDate time.Time) []Fare {
+	var alicanteToModlin FlightResponse
+	var alicanteToChopin FlightResponse
+	var flightsData []byte
 	flightsData = getRyanFlights(alicanteAirportCode, modlinAirportCode, startDate, endDate)
-	err = json.Unmarshal([]byte(flightsData), &alicanteToModlin)
+	err := json.Unmarshal([]byte(flightsData), &alicanteToModlin)
 	if err != nil {
 		log.Fatal("Error unmarshalling JSON:", err)
 	}
@@ -56,76 +72,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error unmarshalling JSON:", err)
 	}
-
-	warsawToAlicanteFares := append(modlinToAlicante.Fares, chopinToAlicante.Fares...)
-	alicanteToWarsawFares := append(alicanteToModlin.Fares, alicanteToChopin.Fares...)
-	convertEURtoPLN(&alicanteToWarsawFares)
-
-	var flightsToCompare []FlightToCompare
-	for _, wawToAlc := range warsawToAlicanteFares {
-		for _, alcToWaw := range alicanteToWarsawFares {
-			departureDate, err := time.Parse("2006-01-02T15:04:05", wawToAlc.Outbound.DepartureDate)
-			if err != nil {
-				log.Fatal(err)
-			}
-			returnDate, err := time.Parse("2006-01-02T15:04:05", alcToWaw.Outbound.DepartureDate)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if departureDate.Before(returnDate) &&
-				returnDate.Sub(departureDate) < time.Hour*24*15 &&
-				returnDate.Sub(departureDate) > time.Hour*24*3 {
-
-				flightsToCompare = append(flightsToCompare, FlightToCompare{wawToAlc.Outbound, alcToWaw.Outbound})
-			}
-		}
-	}
-
-	sort.Slice(flightsToCompare, func(i, j int) bool {
-		priceSummary := flightsToCompare[i].AbroadFlight.Price.Value + flightsToCompare[i].ReturnFlight.Price.Value
-		nextPriceSummary := flightsToCompare[j].AbroadFlight.Price.Value + flightsToCompare[j].ReturnFlight.Price.Value
-		return priceSummary < nextPriceSummary
-	})
-
-	var message bytes.Buffer
-	for _, trip := range flightsToCompare[:10] {
-		log.Print("|", trip.AbroadFlight.DepartureDate, "---", trip.ReturnFlight.DepartureDate)
-		log.Print("|", trip.AbroadFlight.Price.Value+trip.ReturnFlight.Price.Value)
-		log.Print("+-------------------------------------------+")
-		message.WriteString(fmt.Sprintf("%s ---> %s ", trip.AbroadFlight.DepartureAirport.Name, trip.AbroadFlight.ArrivalAirport.Name))
-		message.WriteString(fmt.Sprintf("%s ", strings.Replace(trip.AbroadFlight.DepartureDate, "T", " ", 1)))
-		message.WriteString(fmt.Sprintf("%s%s\n", strconv.FormatFloat(trip.AbroadFlight.Price.Value, 'f', 2, 64), trip.AbroadFlight.Price.CurrencySymbol))
-		message.WriteString(fmt.Sprintf("%s ---> %s ", trip.ReturnFlight.DepartureAirport.Name, trip.ReturnFlight.ArrivalAirport.Name))
-		message.WriteString(fmt.Sprintf("%s ", strings.Replace(trip.ReturnFlight.DepartureDate, "T", " ", 1)))
-		message.WriteString(fmt.Sprintf("%s%s\n", strconv.FormatFloat(trip.ReturnFlight.Price.Value, 'f', 2, 64), trip.ReturnFlight.Price.CurrencySymbol))
-		message.WriteString(fmt.Sprintf("Razem: %szł\n", strconv.FormatFloat(trip.AbroadFlight.Price.Value+trip.ReturnFlight.Price.Value, 'f', 2, 64)))
-		message.WriteString("\n")
-	}
-
-	u := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
-	data := url.Values{}
-	data.Set("chat_id", chatId)
-	data.Set("text", message.String())
-	req, err := http.NewRequest("POST", u, strings.NewReader(data.Encode()))
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	client := http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Error sending request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		log.Print("Message sent successfully!")
-	} else {
-		log.Fatalf("Failed to send message. Status code: %d\n", resp.StatusCode)
-	}
-
+	return append(alicanteToModlin.Fares, alicanteToChopin.Fares...)
 }
 
 func getRyanFlights(
@@ -198,4 +145,84 @@ func getEuroRate() float64 {
 	}
 
 	return exchangeRates.Rates[0].Ask
+}
+
+func getFlightsToCompare(warsawToAlicanteFares, alicanteToWarsawFares []Fare) map[time.Month][]FlightToCompare {
+	flights := make(map[time.Month][]FlightToCompare)
+	for _, wawToAlc := range warsawToAlicanteFares {
+		for _, alcToWaw := range alicanteToWarsawFares {
+			departureDate, err := time.Parse("2006-01-02T15:04:05", wawToAlc.Outbound.DepartureDate)
+			if err != nil {
+				log.Fatal(err)
+			}
+			returnDate, err := time.Parse("2006-01-02T15:04:05", alcToWaw.Outbound.DepartureDate)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if departureDate.Before(returnDate) &&
+				returnDate.Sub(departureDate) < time.Hour*24*15 &&
+				returnDate.Sub(departureDate) > time.Hour*24*3 {
+
+				flights[departureDate.Month()] = append(
+					flights[departureDate.Month()], FlightToCompare{wawToAlc.Outbound, alcToWaw.Outbound})
+			}
+		}
+	}
+	return flights
+}
+
+func buildMessage(now time.Time, flightsToCompare map[time.Month][]FlightToCompare) bytes.Buffer {
+	upcomingMonths := make([]time.Month, lookForwardInMonths)
+	for i := 0; i < lookForwardInMonths; i++ {
+		upcomingMonths[i] = now.AddDate(0, i, -1).Month()
+	}
+
+	var message bytes.Buffer
+	for _, month := range upcomingMonths {
+		sort.Slice(flightsToCompare[month], func(i, j int) bool {
+			priceSummary := flightsToCompare[month][i].AbroadFlight.Price.Value + flightsToCompare[month][i].ReturnFlight.Price.Value
+			nextPriceSummary := flightsToCompare[month][j].AbroadFlight.Price.Value + flightsToCompare[month][j].ReturnFlight.Price.Value
+			return priceSummary < nextPriceSummary
+		})
+		message.WriteString(month.String())
+		message.WriteString("\n")
+		for _, trip := range flightsToCompare[month][:offersPerMonth] {
+			message.WriteString(fmt.Sprintf("%s ---> %s ", trip.AbroadFlight.DepartureAirport.Name, trip.AbroadFlight.ArrivalAirport.Name))
+			message.WriteString(fmt.Sprintf("%s ", strings.Replace(trip.AbroadFlight.DepartureDate, "T", " ", 1)))
+			message.WriteString(fmt.Sprintf("%s%s\n", strconv.FormatFloat(trip.AbroadFlight.Price.Value, 'f', 2, 64), trip.AbroadFlight.Price.CurrencySymbol))
+			message.WriteString(fmt.Sprintf("%s ---> %s ", trip.ReturnFlight.DepartureAirport.Name, trip.ReturnFlight.ArrivalAirport.Name))
+			message.WriteString(fmt.Sprintf("%s ", strings.Replace(trip.ReturnFlight.DepartureDate, "T", " ", 1)))
+			message.WriteString(fmt.Sprintf("%s%s\n", strconv.FormatFloat(trip.ReturnFlight.Price.Value, 'f', 2, 64), trip.ReturnFlight.Price.CurrencySymbol))
+			message.WriteString(fmt.Sprintf("Razem: %szł\n", strconv.FormatFloat(trip.AbroadFlight.Price.Value+trip.ReturnFlight.Price.Value, 'f', 2, 64)))
+			message.WriteString("\n")
+		}
+		message.WriteString("--------------------------------\n")
+	}
+	return message
+}
+
+func sendMessageToTelegram(message bytes.Buffer, botToken, chatId string) {
+	u := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
+	data := url.Values{}
+	data.Set("chat_id", chatId)
+	data.Set("text", message.String())
+	req, err := http.NewRequest("POST", u, strings.NewReader(data.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		log.Print("Message sent successfully!")
+	} else {
+		log.Fatalf("Failed to send message. Status code: %d\n", resp.StatusCode)
+	}
 }
